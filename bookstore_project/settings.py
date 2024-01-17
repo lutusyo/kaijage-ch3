d5a099d46fdc91ee4daf27b2a47cfcbc7bb34f3b
lutusyo <boni>
2024-01-17 10:35:45 +0300

    ch8

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5+zhkc$-=)8v5ka+f-g6x+^2*%h1pmc)@p$v-akntaw)*0a+si'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
